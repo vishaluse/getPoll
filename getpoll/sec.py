980bd82a0d9da4b2b0dafedb5fc211bc175a732a
Vishal Vishwakarma <vishwakarmavishal1999@gmail.com>
2022-02-11 21:25:11 +0530

    third commit

---
+++ getpoll/sec.py
@@ -0,0 +1,10 @@
+#secret key
+sECRET_KEY = 'django-insecure-y=-9kdob2))6bxanwsrk73%tm04&m3!c4@of16dy6yfuvtvp*s'
+
+#smtp info to send mails
+eMAIL_HOST = 'smtp.gmail.com'
+eMAIL_USE_TLS = True
+eMAIL_PORT = 587
+eMAIL_HOST_USER = 'vishwakarmavishal1999@gmail.com'
+eMAIL_HOST_PASSWORD = '11091999'
+dEFAULT_FROM_EMAIL = eMAIL_HOST_USER
