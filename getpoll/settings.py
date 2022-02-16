a593ec111e7d6deeb46e917312d8d5a5ddc61a85
Vishal Vishwakarma <vishwakarmavishal1999@gmail.com>
2022-02-16 13:10:55 +0530

    changed UI working

--- getpoll/settings.py
+++ getpoll/settings.py
@@ -80,17 +80,7 @@ AUTHENTICATION_BACKENDS = (
     "allauth.account.auth_backends.AuthenticationBackend"
 )
 
-SOCIALACCOUNT_PROVIDERS = {
-    'google': {
-        'SCOPE': [
-            'profile',
-            'email',
-        ],
-        'AUTH_PARAMS': {
-            'access_type': 'offline',
-        }
-    }
-}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
